2ae62b72cc9da9f39c50db4c2d738ac4d76d71ce
devilwjp <9700616@qq.com>
2022-04-19 01:10:46 +0800

    👽🪂🚀👽🍟

--- global.d.ts
+++ global.d.ts
@@ -1,18 +1,14 @@
-/// <reference path="@dcloudio/types/uni-app/uni.d.ts" />
 declare namespace UniCrazyGlobalTypes {
     interface UniCrazyRouterParams {
         passedParams?: object | null;
         routeParams?: object | null;
     }
-    interface UniJumpOptions {
-        redirectTo: UniApp.RedirectToOptions;
-        reLaunch: UniApp.ReLaunchOptions;
-        switchTab: UniApp.SwitchTabOptions;
-        navigateBack: UniApp.NavigateBackOptions;
-        navigateTo: UniApp.NavigateToOptions;
-    }
-    type TypeJumpFunction <OPT> = (options: OPT & UniCrazyRouterParams) => void;
-    type MergeUniJump = {[P in keyof UniJumpOptions]: TypeJumpFunction <UniJumpOptions[P]>};
-    type MergeUni = Omit <UniApp.Uni, keyof UniJumpOptions> & MergeUniJump;
 }
-declare const uni: UniCrazyGlobalTypes.MergeUni;
+
+declare namespace UniApp {
+    interface RedirectToOptions extends UniCrazyGlobalTypes.UniCrazyRouterParams {}
+    interface ReLaunchOptions extends UniCrazyGlobalTypes.UniCrazyRouterParams {}
+    interface SwitchTabOptions extends UniCrazyGlobalTypes.UniCrazyRouterParams {}
+    interface NavigateBackOptions extends UniCrazyGlobalTypes.UniCrazyRouterParams {}
+    interface NavigateToOptions extends UniCrazyGlobalTypes.UniCrazyRouterParams {}
+}
